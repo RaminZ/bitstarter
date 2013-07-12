0ea436bf5b202c79318c5f0f2179095c7b34d2f4
Ramin Zamani <ramin.zamani@gmail.com>
2013-07-12 07:24:30 +0000

    Moved the response.send out of the read function

--- web.js
+++ web.js
@@ -1,15 +1,14 @@
 var express = require('express');
 var fs = require('fs');
-
+var buf = new Buffer();
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
     fs.readFileSync('/index.html', function (err, data) {
 	if (err) throw err;
 	console.log(data);
-	var buf = new Buffer();
-	response.send(buf.toString(data));
     });
+    response.send(buf.toString(data));
 });
 
 var port = process.env.PORT || 5000;
